c63e93f4bbe772093693b65f9a7fdeb7eeb33ef8
thanhnd97 <nguyenthanhat1997@gmail.com>
2018-11-01 11:21:03 +0700

    update grade

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.6 on 2018-10-31 14:27:04.
+ * Generated for Laravel 5.7.6 on 2018-11-01 10:35:51.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
--- app/Http/Controllers/Admin/GradeController.php
+++ app/Http/Controllers/Admin/GradeController.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Grade;
 use Illuminate\Http\Request;
 use Illuminate\Http\ResPonse;
 use App\PI_Amount;
 use Illuminate\Database\Eloquent\Model;
+use App\Http\Controllers\Controller;
 
-class GradeController extends MasterController
+class GradeController extends Controller
 {
     //
     function __construct()
@@ -39,5 +40,24 @@ class GradeController extends MasterController
         $grade->name =$request->name;
         $grade->save();
         return redirect('admin/grade/add')->with('message','Thêm thành công');
+    }
+
+    public function getGrade(Grade $grade)
+    {
+
+        return view('admin.grade.edit',compact('grade'));
+    }
+
+    public  function  setName(Request $request,Grade $grade)
+    {
+        $this->validate($request,[
+            'name' => 'required'
+        ],
+        [
+            'name.required' => 'Bạn chưa nhập tên'
+        ]);
+        $grade->name=$request->name;
+        $grade->save();
+        return redirect()->route('admin.grade.list')->with('message','Sửa tên thành công');
     }
 }
